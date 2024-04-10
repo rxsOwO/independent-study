aaa14e6ea95098c39fbecb97efa683abf6c275ec
rxsOwO <91576107+rxsOwO@users.noreply.github.com>
2024-04-09 22:41:11 -0400

    Block Movement
    improvement in block movement

--- GhostGame/Assets/BlockMovement.cs
+++ GhostGame/Assets/BlockMovement.cs
@@ -5,30 +5,129 @@ using UnityEngine;
 public class BlockMovement : MonoBehaviour
 {
     public float time = 0f;
-    public GridLayout gridLayout;
+    public Grid gridLayout;
     Vector3Int cellPosition;
+    Vector3 lerpPosition;
+    bool lerpGo = false;
+    Vector3 startPos;
+    float startTime;
+    float journeyLength;
+    float speed = 10f;
+    public string typename;
     // Start is called before the first frame update
     void Start()
     {
         
         cellPosition = gridLayout.WorldToCell(transform.position);
-        transform.position = gridLayout.CellToWorld(cellPosition);
+        Debug.Log(cellPosition);
+        transform.position = gridLayout.GetCellCenterWorld(cellPosition);
+        Debug.Log(transform.position);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(lerpGo) {
+            journeyLength = Vector3.Distance(startPos, lerpPosition);
+            float distCovered = (Time.time - startTime) * speed;
+            float fractionOfJourney = distCovered / journeyLength;
+            transform.position = Vector3.Lerp(startPos, lerpPosition, fractionOfJourney);
+
+            if(fractionOfJourney == 1f) {
+                lerpGo = false;
+            }
+        }
     }
     void OnCollisionEnter2D (Collision2D other) {
         time = 0f;
     }
+    public bool Ray(Vector2 dir) {
+        RaycastHit2D hit;
+        return hit = Physics2D.Raycast(transform.position, dir, 2f);
+        
+    }
     void OnCollisionStay2D(Collision2D other) {
-        time += Time.deltaTime;
+        if(Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.D)) {
+            time += Time.deltaTime;
+        }
+        else {
+            time = 0f;
+        }
+        
+        
+        if(time>= 0.5f && other.gameObject.name == typename) {
+            bool can = true;
+            if(other.transform.position.x > transform.position.x+0.5 && can) { 
+                time=0f;               
+                can=false;
+                Debug.DrawRay(new Vector2(transform.position.x-0.5f, transform.position.y), Vector2.left, Color.red);
+                RaycastHit2D hit = Physics2D.Raycast(new Vector2(transform.position.x-0.5f, transform.position.y), Vector2.left, 0.5f);
+                Debug.Log(hit.transform);
+                if(!hit || hit.transform.gameObject.name == "Tilemap_" +typename + "Door") {
+                cellPosition = cellPosition+ Vector3Int.left;
+                //transform.position = gridLayout.GetCellCenterWorld(cellPosition);
 
-        if(time>= 0.5f) {
-            transform.position = gridLayout.CellToWorld(new Vector3Int(cellPosition.x +1, cellPosition.y, cellPosition.z));
-            cellPosition = gridLayout.WorldToCell(transform.position);
+
+                lerpGo = true;
+                startPos = transform.position;
+                lerpPosition = gridLayout.GetCellCenterWorld(cellPosition);
+                startTime = Time.time;
+                }
+
+
+            }
+            if(other.transform.position.x < transform.position.x -0.5&& can) {
+                time=0f;
+                Debug.DrawRay(new Vector2(transform.position.x+0.5f, transform.position.y), Vector2.right, Color.red);
+                RaycastHit2D hit = Physics2D.Raycast(new Vector2(transform.position.x+0.5f, transform.position.y), Vector2.right, 0.5f);
+                Debug.Log(hit.transform);
+                can=false;
+                if(!hit || hit.transform.gameObject.name == "Tilemap_" +typename + "Door") {
+                cellPosition = cellPosition+ Vector3Int.right;
+                //transform.position = gridLayout.GetCellCenterWorld(cellPosition);
+                
+                
+                lerpGo = true;
+                startPos = transform.position;
+                lerpPosition = gridLayout.GetCellCenterWorld(cellPosition);
+                startTime = Time.time;
+                }
+            }
+            if(other.transform.position.y < transform.position.y-0.5 && can) {
+                time=0f;
+                Debug.DrawRay(new Vector2(transform.position.x, transform.position.y+0.5f), Vector2.up, Color.red);
+                RaycastHit2D hit = Physics2D.Raycast(new Vector2(transform.position.x, transform.position.y+0.5f), Vector2.up, 0.5f);
+                Debug.Log(hit.transform);
+                can=false;
+                if(!hit || hit.transform.gameObject.name == "Tilemap_" +typename + "Door") {
+                cellPosition = cellPosition+ Vector3Int.up;
+                //transform.position = gridLayout.GetCellCenterWorld(cellPosition);
+                
+
+                lerpGo = true;
+                startPos = transform.position;
+                lerpPosition = gridLayout.GetCellCenterWorld(cellPosition);
+                startTime = Time.time;
+                }
+            }
+            if(other.transform.position.y > transform.position.y+0.5 && can) {
+                time=0f;
+                Debug.DrawRay(new Vector2(transform.position.x, transform.position.y-0.5f), Vector2.down, Color.red);
+                RaycastHit2D hit = Physics2D.Raycast(new Vector2(transform.position.x, transform.position.y-0.5f), Vector2.down, 0.5f);
+                Debug.Log(hit.transform);
+                can=false;
+                if(!hit || hit.transform.gameObject.name == "Tilemap_" +typename + "Door") {
+                cellPosition = cellPosition+ Vector3Int.down;
+                //transform.position = gridLayout.GetCellCenterWorld(cellPosition);
+                
+
+                lerpGo = true;
+                startPos = transform.position;
+                lerpPosition = gridLayout.GetCellCenterWorld(cellPosition);
+                startTime = Time.time;
+                }
+            }
+        
         }
     }
 }
