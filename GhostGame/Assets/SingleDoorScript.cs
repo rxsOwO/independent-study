b59abe9d0e6b4847c64120cc7c1bbff0ec4ac5dc
rxsOwO <91576107+rxsOwO@users.noreply.github.com>
2024-05-05 23:05:55 -0400

    pp

---
+++ GhostGame/Assets/SingleDoorScript.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SingleDoorScript : MonoBehaviour
+{
+    public bool hasBloopy;
+    public GameObject thing;
+    public GameObject door;
+
+    public AudioSource audio;
+
+    public bool Open;
+
+    void Update() {
+        if(hasBloopy) {
+            thing.SetActive(true);
+        }
+        if(hasBloopy) {
+            door.SetActive(false);
+            if(!Open) {
+                audio.Play();
+                Open = true;
+            }
+        }
+        else {
+            Open = false;
+            door.SetActive(true);
+        }
+    }
+}
