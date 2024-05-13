01b7afb72d0f1539c27938f2d2e57562993be226
rxsOwO <91576107+rxsOwO@users.noreply.github.com>
2024-05-12 22:38:15 -0400

    end probably

--- GhostGame/Assets/EndLevel.cs
+++ GhostGame/Assets/EndLevel.cs
@@ -7,6 +7,8 @@ public class EndLevel : MonoBehaviour
 {
     public Rigidbody2D BloopyRb;
     public Rigidbody2D GloopyRb;
+    public bool isEnd;
+    public GameObject text;
     void Start()
     {
     }
@@ -30,13 +32,21 @@ public class EndLevel : MonoBehaviour
 
         BloopyAnim.Play("Player_Death");
         GloopyAnim.Play("Player_Death");
+        if(isEnd) {
+            text.SetActive(true);
+        }
         Invoke("EndMenuActivate", 1.2f);
     }
     public void EndMenuActivate() {
-        PlayerPrefs.SetInt("lastSavedScene", SceneManager.GetActiveScene().buildIndex + 1);
-        PlayerPrefs.SetInt("highestLevel", SceneManager.GetActiveScene().buildIndex - 1);
-        BloopyRb.bodyType = RigidbodyType2D.Static;
-        GloopyRb.bodyType = RigidbodyType2D.Static;
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        if(!isEnd) {
+            PlayerPrefs.SetInt("lastSavedScene", SceneManager.GetActiveScene().buildIndex + 1);
+            PlayerPrefs.SetInt("highestLevel", SceneManager.GetActiveScene().buildIndex - 1);
+            BloopyRb.bodyType = RigidbodyType2D.Static;
+            GloopyRb.bodyType = RigidbodyType2D.Static;
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);  
+        }
+        else {
+            SceneManager.LoadScene(0);
+        }
     }
 }
--- GhostGame/Assets/PlayerMovement.cs
+++ GhostGame/Assets/PlayerMovement.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerMovement : MonoBehaviour
 {
@@ -17,7 +18,15 @@ public class PlayerMovement : MonoBehaviour
         animator.SetFloat("Horizontal", movement.x);
         animator.SetFloat("Vertical", movement.y);
         animator.SetFloat("Speed", movement.magnitude);
-
+        if(Input.GetKey(KeyCode.Escape)) {
+            PlayerPrefs.SetInt("lastSavedScene", SceneManager.GetActiveScene().buildIndex);
+            
+            if(PlayerPrefs.GetInt("highestLevel") <= SceneManager.GetActiveScene().buildIndex - 2) {
+                PlayerPrefs.SetInt("highestLevel", SceneManager.GetActiveScene().buildIndex - 2);
+            }
+            
+            SceneManager.LoadScene(0);
+        }
     }
     void FixedUpdate()
     {
